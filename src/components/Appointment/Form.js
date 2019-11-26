0c961ad79e13d39a6f652984e602f4cc1bc07063
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-11-26 12:57:08 -0800

    add appointment components, button day list and interview list

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  return <main className="appointment__card appointment__card--create">
+    <section className="appointment__card-left">
+      <form autoComplete="off">
+        <input
+          className="appointment__create-input text--semi-bold"
+          name={name}
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          placeholder="Enter Student Name"
+          onSubmit={(event) => event.preventDefault()}
+        />
+      </form>
+      <InterviewerList
+        interviewers={props.interviewers}
+        interviewer={interviewer}
+        setInterviewer={setInterviewer}
+      />
+    </section>
+    <section className="appointment__card-right">
+      <section className="appointment__actions">
+        <Button danger onClick={(event) => {
+          setName('');
+          setInterviewer(null);
+          props.onCancel();
+        }}>Cancel</Button>
+        <Button confirm onClick={(event) => {
+          setName('');
+          setInterviewer(null);
+          props.onSave(name, interviewer);
+        }}>Save</Button>
+      </section>
+    </section>
+  </main>
+}
