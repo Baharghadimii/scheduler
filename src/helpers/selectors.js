8206824e721532019ed7211fa3267b5f9fe6f06d
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-11-28 15:22:46 -0800

    add test for getInterviewersForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,12 +3,14 @@ const state = {
     {
       id: 1,
       name: "Monday",
-      appointments: [1, 2, 3]
+      appointments: [1, 2, 3],
+      interviewers: [1, 2, 3]
     },
     {
       id: 2,
       name: "Tuesday",
-      appointments: [4, 5]
+      appointments: [4, 5],
+      interviewers: [4, 5]
     }
   ],
   appointments: {
@@ -36,7 +38,22 @@ const state = {
       id: 2,
       name: "Tori Malcolm",
       avatar: "https://i.imgur.com/Nmx0Qxo.png"
-    }
+    },
+    "3": {
+      id: 3,
+      name: "Mildred Nazir",
+      avatar: "https://i.imgur.com/T2WwVfS.png"
+    },
+    "4": {
+      id: 4,
+      name: "Sven Jones",
+      avatar: "https://i.imgur.com/twYrpay.jpg"
+    },
+    "5": {
+      id: 5,
+      name: "Susan Reynolds",
+      avatar: "https://i.imgur.com/TdOAdde.jpg"
+    },
   }
 
 };
@@ -57,6 +74,18 @@ export function getInterview(state, interview) {
   let result = null;
   if (interview) {
     result = { ...interview, interviewer: state.interviewers[interview.interviewer] };
+  }
+  return result;
+}
+export function getInterviewersForDay(state, d) {
+  let result = [];
+  const days = state.days.filter(day => day.name === d);
+  if (days.length) {
+    days[0].interviewers.forEach(element => {
+      if (state.interviewers[element]) {
+        result.push(state.interviewers[element]);
+      }
+    });
   }
   return result;
 }
