95db428f1fe3af5cdfb842210f5649bfe395e201
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-12-02 23:23:42 -0800

    dd test for api request

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,24 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement } from "@testing-library/react";
 
 import Application from "components/Application";
+import { fireEvent } from "@testing-library/react/dist";
 
 afterEach(cleanup);
+describe(("Application"), () => {
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
 
-it("renders without crashing", () => {
-  render(<Application />);
+    await waitForElement(() => getByText("Monday"));
+
+    fireEvent.click(getByText("Tuesday"));
+
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+  it('loads data, books an interview and reduces the spots remaining for the first day by 1', async () => {
+    const { getByText } = render(<Application />);
+    await waitForElement(() => getByText("Archie Cohen"));
+  })
 });
+
