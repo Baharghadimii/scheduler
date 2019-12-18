3e83f3759cbcf108e3134aedb4ae2a5cf18a8449
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-12-18 13:58:36 -0800

    test

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -5,8 +5,8 @@ export const SET_INTERVIEW = "SET_INTERVIEW";
 //reducer hook
 export default function reducer(state, action) {
   switch (action.type) {
-    // case SET_DAY:
-    //   return { ...state, day: action.day }
+    case SET_DAY:
+      return { ...state, day: action.day }
     case SET_APPLICATION_DATA:
       return { ...state, days: action.days, appointments: action.appointments, interviewers: action.interviewers }
     case SET_INTERVIEW:
